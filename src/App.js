f8c59e6dca8360a056311ceb702a050da64a4ec2
dhruvitpatel <dhruvitpatel@knovator.in>
2021-08-31 22:14:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/vendor/bootstrap/css/bootstrap.min.css';
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
+import './assets/vendor/boxicons/css/boxicons.min.css';
+import './assets/vendor/fontawesome-free/css/all.min.css';
+import './assets/vendor/remixicon/remixicon.css';
+import './assets/css/style.css';
+import React from 'react';
+import HeaderMain from './component/Header';
+import Footer from './component/Footer';
+import HomePage from './component/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeaderMain/>
+      <HomePage/>
+      <Footer/>
+    </>
   );
 }
 
