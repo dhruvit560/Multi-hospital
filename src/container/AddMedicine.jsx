0a8b82aad02b9c9f34f4d9f9041736cf5b51aca0
dhruvitpatel <dhruvitpatel@knovator.in>
2021-09-13 21:30:09 +0530

    dynamic form in add and remove complete

--- src/container/AddMedicine.jsx
+++ src/container/AddMedicine.jsx
@@ -2,43 +2,37 @@ import React, { useState } from "react";
 import SingleMedicine from "../component/SingleMedicine";
 
 const AddMedicine = () => {
-  const [inputFields, setInputFields] = useState([
-    {
-      name: "",
-      quality: "",
-      expiry: "",
-      details: "",
-    }
+  const [inputField, setInputField] = useState([
+    { name: "", quality: "", expiry: "", details: "" },
   ]);
 
-  const handleChanges = (e, index) => {
-   
-    const values = [...inputFields];
-    if(e.target.name == "name"){
-        values[index].name = e.target.value;
-    } else if(e.target.name == "quality"){
-      values[index].quality = e.target.value;
-    } else if(e.target.name == "expiry"){
-      values[index].expiry = e.target.value;
-    } else if(e.target.name == "details"){
-      values[index].details = e.target.value;
-    }
-    
-  };
+  const addInput = (e,index) => {
+    const values = [...inputField];
+    values.push({ name: "", quality: "", expiry: "", details: "" })
 
-  const addInputField = () => {
-    const values = [...inputFields];
-    values.push({ name: "", quality: "", expiry: "", details: "" });
-
-    setInputFields(values);
-  };
-  const removeInputField = (index) => {
-    const values = [...inputFields];
+    setInputField(values);
+  } 
+  const removeInput = (e,index) => {
+    const values = [...inputField];
     values.splice(index,1);
 
-    setInputFields(values);
+    setInputField(values);
   };
+  const handleChanges = (e,index) => {
+    const values = [...inputField];
+    
+    if(e.target.name == "name"){
+       values[index].name = e.target.value;
+    } else if(e.target.name == "quality"){
+      values[index].quality = e.target.value;
+    }else if(e.target.name == "expiry"){
+      values[index].expiry = e.target.value;
+    }else if(e.target.name == "details"){
+      values[index].details = e.target.value;
+    }
 
+    setInputField(values)
+  };
   const submitForm = () => {};
   return (
     <>
@@ -49,72 +43,63 @@ const AddMedicine = () => {
             method="post"
             onSubmit={(e) => submitForm(e)}
           >
-            
-            {inputFields.map((value, index) => {
+            {inputField.map((e, index) => {
               return (
-                <>
-                  <div className="row">
-                    <div className="col-1">
-                      {index}
-                    </div>
-                    <div className="col-lg-2 col-6">
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Name"
-                        name="name"
-                        onChange={(e) => handleChanges(e)}
-                        value={value.name}
-                      />
-                    </div>
-                    <div className="col-lg-2 col-6">
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Quality"
-                        name="quality"
-                        onChange={(e) => handleChanges(e)}
-                        value={value.quality}
-                      />
-                    </div>
-                    <div className="col-lg-2 col-6">
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Expire Date"
-                        name="expiry"
-                        onChange={(e) => handleChanges(e)}
-                        value={value.expiry}
-                      />
-                    </div>
-                    <div className="col-lg-2 col-6">
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Details"
-                        name="details"
-                        onChange={(e) => handleChanges(e)}
-                        value={value.details}
-                      />
-                    </div>
-                    <div className="col-lg-2 col-6">
-                      <a
-                        href="#"
-                        className="px-3 mx-2 btn btn-success d-inline-block"
-                        onClick={addInputField}
-                      >
-                        +
-                      </a>
-                      <a
-                        href="#"
-                        className="px-3 btn btn-danger d-inline-block"
-                        onClick={removeInputField}
-                      >
-                        -
-                      </a>
-                    </div>
+                <div className="mt-3 row">
+                  <div className="col-1">{index + 1}</div>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Name"
+                      name="name"
+                      onChange={(e) => handleChanges(e,index)}
+                      value={e.name}
+                    />
                   </div>
-                </>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Quality"
+                      name="quality"
+                      onChange={(e) => handleChanges(e,index)}
+                      value={e.quality}
+                    />
+                  </div>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Expire Date"
+                      name="expiry"
+                      onChange={(e) => handleChanges(e,index)}
+                      value={e.expiry}
+                    />
+                  </div>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Details"
+                      name="details"
+                      onChange={(e) => handleChanges(e,index)}
+                      value={e.details}
+                    />
+                  </div>
+                  <div className="col-lg-2 col-6">
+                    <a
+                      href="#"
+                      className="px-3 mx-2 btn btn-success d-inline-block"
+                      onClick={(e) => addInput(e,index)}
+                    >
+                      +
+                    </a>
+                    <a href="#" className="px-3 btn btn-danger d-inline-block" onClick={(e) => removeInput(e, index)}>
+                      -
+                    </a>
+                  </div>
+                </div>
               );
             })}
           </form>
