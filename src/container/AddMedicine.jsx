159c9ab0397ba6957208d2e8ba04e83129975026
dhruvitpatel <dhruvitpatel@knovator.in>
2021-09-16 20:41:05 +0530

    add and remove isn't working properly

--- src/container/AddMedicine.jsx
+++ src/container/AddMedicine.jsx
@@ -12,20 +12,20 @@ const AddMedicine = () => {
   ]);
 
   const handleChanges = (e) => {
-    // console.log(e.target.value + " " + e.target.name);
+    console.log(e.target.value + " " + e.target.name);
   };
 
-  const addInput = (e,index) => {
+  const addInput = (e, index) => {
     const values = [...inputFields];
-    values.splice(index , 0 ,{name: "",quality: "",expiry: "",details: ""})
-    setInputFields(values)
+    values.splice(index, 0, { name: "", quality: "", expiry: "", details: "" });
+    setInputFields(values);
     // console.log(index);
-  } 
-  const removeInput = (e,index) => {
+  };
+  const removeInput = (e, index) => {
     const values = [...inputFields];
-    values.splice(index , 1)
-    setInputFields(values)
-  } 
+    values.splice(index, 1);
+    setInputFields(values);
+  };
   const submitForm = () => {};
   return (
     <>
@@ -37,59 +37,63 @@ const AddMedicine = () => {
             onSubmit={(e) => submitForm(e)}
           >
             {inputFields.map((e, index) => {
-              return(
+              return (
                 <div className="mt-3 row">
-                <div className="col-1">{index + 1}</div>
-                <div className="col-lg-2 col-6">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Name"
-                    name="name"
-                    onChange={(e) => handleChanges(e)}
-                  />
+                  <div className="col-1">{index + 1}</div>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Name"
+                      name="name"
+                      onChange={(e) => handleChanges(e)}
+                    />
+                  </div>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Quality"
+                      name="quality"
+                      onChange={(e) => handleChanges(e)}
+                    />
+                  </div>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Expire Date"
+                      name="expiry"
+                      onChange={(e) => handleChanges(e)}
+                    />
+                  </div>
+                  <div className="col-lg-2 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Details"
+                      name="details"
+                      onChange={(e) => handleChanges(e)}
+                    />
+                  </div>
+                  <div className="col-lg-2 col-6">
+                    <a
+                      href="#"
+                      className="px-3 mx-2 btn btn-success d-inline-block"
+                      onClick={(e) => addInput(e, index)}
+                    >
+                      +
+                    </a>
+                    <a
+                      onClick={(e) => removeInput(e, index)}
+                      href="#"
+                      className="px-3 btn btn-danger d-inline-block"
+                    >
+                      -
+                    </a>
+                  </div>
                 </div>
-                <div className="col-lg-2 col-6">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Quality"
-                    name="quality"
-                    onChange={(e) => handleChanges(e)}
-                  />
-                </div>
-                <div className="col-lg-2 col-6">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Expire Date"
-                    name="expiry"
-                    onChange={(e) => handleChanges(e)}
-                  />
-                </div>
-                <div className="col-lg-2 col-6">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Details"
-                    name="details"
-                    onChange={(e) => handleChanges(e)}
-                  />
-                </div>
-                <div className="col-lg-2 col-6">
-                  <a
-                    href="#"
-                    className="px-3 mx-2 btn btn-success d-inline-block"
-                    onClick={(e) => addInput(e,index)}
-                  >
-                    +
-                  </a>
-                  <a onClick={(e) => removeInput(e, index)} href="#" className="px-3 btn btn-danger d-inline-block">
-                    -
-                  </a>
-                </div>
-              </div>
-              )
+              );
             })}
           </form>
         </div>
